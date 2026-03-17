dc91a39278d873fa731502c486bb7559c017c437
Letizia <106375016+letylet@users.noreply.github.com>
2026-03-17 17:16:22 +0100

    refactor: changed texts + added tags

--- constants.ts
+++ constants.ts
@@ -20,7 +20,7 @@ export const PROJECTS: Project[] = [
       it: 'L’esperienza prevede che inquadrando dei marker (delle foto dei vari elementi di interesse presenti nel museo), compaia un interfaccia che permette la visione di fotografie antiche e la possibilità di ascoltare o leggere la guida relativa al punto di interesse inquadrato. \n\nRESPONSABILITÀ: \nMi sono occupata dello sviluppo dell’interfaccia grafica, della gestione di MindAR per la realtà aumentata e delle interazioni dell’utente.',
       en: 'I collaborated on the creation of an augmented reality audio guide accessible via web for the Bagatti Valsecchi Museum in Milan. The experience involves scanning markers (photos of various elements of interest in the museum) to reveal an interface allowing the viewing of ancient photographs and the ability to listen to or read the guide related to the point of interest. \n\nRESPONSIBILITIES: \nI was responsible for the development of the graphical interface, the user interaction and the management of MindAR for augmented reality.'
     },
-    tags: ['Unity', 'C#', 'WebGL', 'MindAR'],
+    tags: ['Unity', 'C#', 'Web', '2D', 'WebGL', 'MindAR'],
     date: {
       it: 'Aprile 2025 - Giugno 2025',
       en: 'April 2025 - June 2025'
@@ -44,10 +44,10 @@ export const PROJECTS: Project[] = [
       en: 'Collaborated on the development of a 3D game built with Unity.'
     },
     fullDescription: {
-      it: 'IL GIOCO: \nCostruisci e gestisci un ristorante tutto tuo per attirare nuovi avventurieri e potenziare Diavoli e umani con del cibo mostruoso. unisce la strategia dei classici RPG-tattici a elementi del genere roguelike. \n\nRESPONSABILITÀ: \nSviluppo schermate di UI, meccaniche di gioco e bug fixing. \n\nCOSA HO IMPARATO: \n- Importazione e gestione Spine models in Unity \n- Gestione approfondita della UI \n- Capacità di inserirmi in un progetto complesso in uno stato di sviluppo già avanzato \n',
+      it: 'IL GIOCO: \nCostruisci e gestisci un ristorante tutto tuo per attirare nuovi avventurieri e potenziare Diavoli e umani con del cibo mostruoso. unisce la strategia dei classici RPG-tattici a elementi del genere roguelike. \n\nRESPONSABILITÀ: \nSviluppo schermate di UI, meccaniche di gioco e bug fixing. \n\nCOSA HO IMPARATO: \n- Importazione e gestione Spine models in Unity \n- Gestione avanzata della UI \n- Gestione animazioni e sequenze di animazioni con DOTween \n- Capacità di inserirmi in un progetto complesso in uno stato di sviluppo già avanzato \n',
       en: 'THE GAME: \nHunt monsters, loot their spoils and cook some otherworldly dishes! You can customize your own restaurant to attract new human adventurers and power up your Food Devils through hellish contracts! \n\n RESPONSIBILITIES: \nUI, gameplay mechanics and bug fixing. '
     },
-    tags: ['Unity', 'C#', '3D', 'DOTween'],
+    tags: ['Unity', 'C#', 'Desktop', '3D', 'DOTween'],
     date: {
       it: 'Gennaio 2026 - Attualmente',
       en: 'January 2026 - Now'
@@ -74,7 +74,7 @@ export const PROJECTS: Project[] = [
       it: "Ho progettato e realizzato l'applicazione con Unity e Vuforia. L'esperienza prevede l'interazione con i Murales che si trovano nel paese di Sant'Angelo di Roccalvecce. Inquadrando le opere compaiono degli oggetti con i quali interagire e giocare. \n\nOBIETTIVO: \nPermettere ai visitatori di scoprire in modo interattivo più informazioni riguardo l'opera e la storia raccontata. \n\n COSA HO IMPARATO: \n- Gestione realtà aumentata con Unity e Vuforia \n- Particle System in Unity \n- Spawn dinamico di prefab con riutilizzo di gameobject",
       en: 'I designed and developed an AR application using Unity and Vuforia. The experience features interactive engagement with the murals located in the village of Sant’Angelo di Roccalvecce. \n\nGOAL: \nTo enable visitors to discover in-depth information about the paintings and the stories they describe, through an immersive and interactive interface.'
     },
-    tags: ['Unity', 'C#', 'Vuforia', 'AR', 'Game Design'],
+    tags: ['Unity', 'C#','2D', 'Mobile', 'Vuforia', 'AR', 'Game Design'],
     link:"https://drive.google.com/file/d/1_dqF819JV5IGSrOSbAOpxKRDmAT5Ieh-/view",
     date: {
       it: '2022',
@@ -99,10 +99,10 @@ export const PROJECTS: Project[] = [
       en: 'Applied game part of the Erasmus+ KA2 project "Edugaming vs. Radicalisation".'
     },
     fullDescription: {
-      it: "Un platform 2D in cui il giocatore, manovrando il suo ombrello, deve ricercare parole positive nascoste nella scena, evitando una pioggia di parole negative. \n\nRESPONSABILITÀ: \nSviluppo di meccaniche di gioco, realizzazione di animazioni 2D. \n\nCOSA HO IMPARATO: \n- Bone Editor per la realizzazione di animazioni in 2D \n- Gestione dei dialoghi con Ink \n- Shader Graph di Unity per realizzare degli effetti visivi. \n- Gestione e animazione di testi con Text Mesh Pro. \n - Creazione di Particle System. \n- Creazione e utilizzo di Scriptable Object. \n- Creazione sistema di Spawn con riutilizzo di gameObject",
+      it: "Un platform 2D in cui il giocatore, manovrando il suo ombrello, deve ricercare parole positive nascoste nella scena, evitando una pioggia di parole negative. Il gioco è giocabile sia su mobile che su browser desktop.\n\nRESPONSABILITÀ: \nSviluppo di meccaniche di gioco, realizzazione di animazioni 2D. \n\nCOSA HO IMPARATO: \n- Bone Editor per la realizzazione di animazioni in 2D \n- Gestione dei dialoghi con Ink \n- Shader Graph di Unity per realizzare degli effetti visivi. \n- Gestione e animazione di testi con Text Mesh Pro. \n - Creazione di Particle System. \n- Creazione e utilizzo di Scriptable Object. \n- Creazione sistema di Spawn con riutilizzo di gameObject. \n- Gestione input mobile e desktop",
       en: 'A 2D platformer where the player searches for positive words while dodging a rain of negative words by rotating their umbrella. \n\nRESPONSIBILITIES: \nDevelopment of gameplay mechanics, creation of 2D animations.'
     },
-    tags: ['Unity','C#', 'Ink', 'Bone Editor'],
+    tags: ['Unity','C#', '2D', 'Mobile', 'Desktop', 'Ink', 'Bone Editor'],
     images: ['umbrellaPlus.png'],
     date: {
       it: 'Sett 2022 - Dic 2022',
@@ -114,7 +114,7 @@ export const PROJECTS: Project[] = [
     category: ProjectCategory.GAME
   },
   {
-    id: 'ggj-2025',
+    id: 'ggj-2026',
     title: {
       it: 'Global Game Jam 2026',
       en: 'Global Game Jam 2026'
@@ -127,7 +127,7 @@ export const PROJECTS: Project[] = [
       it: "Gioco di carte in 2D ispirato al grande classico: 'Scopa'. Un roguelike single-player in cui si possono usare delle maschere per cambiare il seme o il valore delle carte in mano in modo da fare le prese migliori e sconfiggere l'avversario.",
       en: 'A 2D card game inspired by the Italian classic "Scopa." A single-player roguelike where players can use masks to change the suit or value of the cards in their hand, allowing them to make the best captures and defeat the opponent.'
     },
-    tags: ['Unity', 'C#', 'DOTween', 'Game Design'],
+    tags: ['Unity', 'C#', '2D', 'Desktop', 'DOTween', 'Game Design'],
     images: ['mascaria.png'],
     date: {
       it: 'Gennaio 2026',
@@ -152,7 +152,7 @@ export const PROJECTS: Project[] = [
       it: 'Sviluppo meccaniche di gioco per gioco indie d\'azione/puzzle in 2D.',
       en: 'Development of game mechanics for an indie 2D action/puzzle game.'
     },
-    tags: ['C#', 'Unity'],
+    tags: ['Unity', 'C#', '2D', 'Desktop'],
     images: ['searing.jpg'],
     date: {
       it: '2024',
@@ -172,7 +172,7 @@ export const PROJECTS: Project[] = [
       it: "Applicazione realizzata in C++ e OpenGL. Lavoro di gruppo in cui abbiamo realizzato una stanza (usando una cube map) nella quale muoversi fino al raggiungimento di un portale che trasporta l'utente in un modello 3D del sistema solare. Abbiamo creato la libreria matematica da zero e abbiamo creato e gestito la scena scrivendo il codice per i Vertex e Fragment shader custom usando glsl.",
       en: 'Application developed in C++ and OpenGL. A collaborative project featuring a room (rendered using a cubemap) where the player can move until reaching a portal that transports them to a 3D model of the solar system. We built the math library from scratch and managed the scene by writing custom Vertex and Fragment shaders using GLSL.'
     },
-    tags: ['C++', 'OpenGL', 'GLSL'],
+    tags: ['C++', 'Desktop', 'OpenGL', 'GLSL'],
     sortDate: '2022-02-01',
     type: ProjectType.FIXED,
     category: ProjectCategory.OTHER
@@ -202,7 +202,7 @@ export const PROJECTS: Project[] = [
       it: 'Videogioco Snake con modalità a 2 giocatori e classifica realizzato in Java.',
       en: 'Snake video game with 2-player mode and leaderboard.'
     },
-    tags: ['Java'],
+    tags: ['Java', '2D', 'Desktop'],
     images: ['snake2players.png'],
     type: ProjectType.COMPACT,
     sortDate: '2020-01-01',
